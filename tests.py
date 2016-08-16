a1f98512b506a5cb3e43b9d6ef08bd85d85bbf52
Will Church <willchurch1@gmail.com>
2016-08-16 14:09:58 -0500

    start socketio tests

--- tests.py
+++ tests.py
@@ -4,9 +4,9 @@ from flask import Flask
 #from flask_testing import TestCase, LiveServerTestCase
 
 
-from ratchat import app
+from ratchat import app, socketio
 
-class FlaskBookshelfTests(unittest.TestCase):
+class TestChat(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -17,26 +17,15 @@ class FlaskBookshelfTests(unittest.TestCase):
         pass
 
     def setUp(self):
-        # creates a test client
-        self.app = app.test_client()
-        # propagate the exceptions to the test client
-        self.app.testing = True
+        pass
 
     def tearDown(self):
         pass
 
-    def test_home_status_code(self):
-        # sends HTTP GET request to the application
-        # on the specified path
-        result = self.app.get('/')
-
-        # assert the status code of the response
-        self.assertEqual(result.status_code, 200)
-
-    def test_home_data(self):
-        # sends HTTP GET request to the application
-        # on the specified path
-        result = self.app.get('/')
-
-        # assert the response data
-        self.assertEqual(result.data, b"Hello World!!!") 
+    def test_connect(self):
+        client = socketio.test_client(app)
+        received = client.get_received()
+        print(received)
+        self.assertEqual(len(received), 1)
+        self.assertEqual(received[0]['args'], 'connected')
+        client.disconnect()
